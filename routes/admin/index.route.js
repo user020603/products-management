dbb6e46a1c49eb15177e037b08d55908aba8a721
user020603 <ptit.tienthanh@gmail.com>
2024-01-28 15:24:44 +0700

    1.3 Lam giao dien danh sach san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,9 @@
 const dashboardRouters = require("./dashboard.route");
+const productRouters = require("./product.route");
 const systemCofig = require("../../config/system");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemCofig.prefixAdmin;
     app.use(PATH_ADMIN + "/dashboard", dashboardRouters);
+    app.use(PATH_ADMIN + "/products", productRouters);
 }
