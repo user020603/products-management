03ab617466117b3ae808526748937c8e98c965da
user020603 <ptit.tienthanh@gmail.com>
2024-01-28 17:21:06 +0700

    Tinh nang tim kiem (Front-end)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -18,3 +18,25 @@ if (buttonStatus.length > 0) {
         })
     })
 }
+
+// End Button Status
+
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+    let url = new URL(window.location.href);
+
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value
+        // console.log(keyword);
+        if (keyword != "") {
+            url.searchParams.set("keyword", keyword);
+        }
+        else {
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    });
+}
+// End Form Search
