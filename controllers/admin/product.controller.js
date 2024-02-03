ce5dbb06c371d11a457dfd6e4089ffd8bc24297d
user020603 <ptit.tienthanh@gmail.com>
2024-02-03 22:25:22 +0700

    Xoa mem san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -80,6 +80,8 @@ module.exports.changeMulti = async (req, res) => {
 // [DELETE] /admin/products/deleteItem/:id
 module.exports.deleteItem = async (req, res) => {
     const id = req.params.id;
-    await Product.deleteOne({ _id: id});
+
+    // await Product.deleteOne({ _id: id});
+    await Product.updateOne({ _id: id}, { deleted: true, deletedAt: new Date() });
     res.redirect("back");
 }
