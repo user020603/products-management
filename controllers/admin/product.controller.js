93128a44985efbba56a4609f39a973a5be5e1dfb
user020603 <ptit.tienthanh@gmail.com>
2024-02-05 04:56:55 +0700

    Tinh nang tao moi san pham (Upload anh)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -118,6 +118,7 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products/create
 module.exports.createPost = async (req, res) => {
+    // console.log(req.file);
     req.body.price = parseInt(req.body.price);
     req.body.discountPercentage = parseInt(req.body.discountPercentage);
     req.body.stock = parseInt(req.body.stock);
@@ -130,6 +131,7 @@ module.exports.createPost = async (req, res) => {
     }
     // console.log(req.body);
 
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
     const product = new Product(req.body);
     await product.save();
 
