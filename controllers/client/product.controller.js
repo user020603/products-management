d7f31421cdf79eaee4975e5cb87428b78630a001
user020603 <ptit.tienthanh@gmail.com>
2024-02-08 23:59:21 +0700

    Lam trang chi tiet san pham ben Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,22 +1,45 @@
 const Product = require("../../models/product.model");
 
 // [GET] /products
-
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        status: "active",
-        deleted: false
-    }).sort({ position: "desc" });
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
 
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
-        return item;
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
+
+  console.log(newProducts);
+
+  res.render("client/pages/products/index.pug", {
+    pageTitle: "Danh sach san pham",
+    products: newProducts,
+  });
+};
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+        deleted: false, 
+        slug: req.params.slug,
+        status: "active"
+    };
+
+    const product = await Product.findOne(find);
+    
+    res.render(`client/pages/products/detail`, {
+        pageTitle: "Chi tiet san pham",
+        product: product
     });
-
-    console.log(newProducts);
-
-    res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sach san pham",
-        products: newProducts
-    });
+  }
+  catch {
+    res.redirect(`/products`);
+  }
 };
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,8 +5,6 @@ const controller = require("../../controllers/client/product.controller");
 
 router.get("/", controller.index); 
 
-router.get("/edit", (req, res) => {
-    res.send("<h1> Day la trang chinh sua </h1>");
-});
+router.get("/:slug", controller.detail);
 
 module.exports = router;
