7c92c78f689da71114f58ca7635a4c5ba0868dc9
user020603 <ptit.tienthanh@gmail.com>
2024-02-16 01:38:05 +0700

    Tinh nang chinh sua danh muc san pham

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -48,3 +48,47 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false
+    })
+  
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+  
+    const newRecords = createTreeHelper.tree(records);
+  
+  
+    console.log(data);
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    });
+  }
+  catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+// [PATCH] /admin/products-category/editPatch/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  console.log(id)
+  console.log(req.body)
+
+  req.body.position = parseInt(req.body.position)
+
+  await ProductCategory.updateOne({ _id: id}, req.body)
+
+  res.redirect(`back`);
+
+};
