452abf55f58402fc37be2eb4d8653d3e7bd89b4b
user020603 <ptit.tienthanh@gmail.com>
2024-02-16 10:49:49 +0700

    Chinh sua nhom quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -5,33 +5,65 @@ const systemConfig = require("../../config/system");
 // [GET] /admin/roles/
 module.exports.index = async (req, res) => {
   const records = await Role.find({
-    deleted: false
+    deleted: false,
   });
 
   res.render("admin/pages/roles/index", {
     pageTitle: "Nhóm quyền",
-    records: records
+    records: records,
   });
-}
+};
 
 // [GET] /admin/roles/create
 module.exports.create = (req, res) => {
-    res.render("admin/pages/roles/create", {
-      pageTitle: "Thêm mới Nhóm quyền"
-    });
-}
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Thêm mới Nhóm quyền",
+  });
+};
 
 // [POST] /admin/roles/create
 module.exports.createPost = async (req, res) => {
   try {
     const record = new Role(req.body);
     await record.save();
-  
+
     req.flash("success", "Thêm mới thành công!");
-  
+
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  } catch (error) {
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
   }
-  catch (error) {
+};
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+    const data = await Role.findOne(find);
+    // console.log(data);
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data,
+    });
+  } catch {
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
   }
-}
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "Cập nhật thành công!");
+  } catch (error) {
+    req.flash("error", "Cập nhật thất bại!");
+  }
+  res.redirect(`back`);
+};
