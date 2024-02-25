cef80b41da5102da4779fa7325b699f740c51373
user020603 <ptit.tienthanh@gmail.com>
2024-02-25 17:52:15 +0700

    Lao giao dien thanh toan

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,7 @@ const productRouters = require("./product.route.js");
 const homeRouters = require("./home.route.js");
 const searchRouters = require("./search.route.js");
 const cartRouters = require("./cart.route.js");
+const checkoutRouters = require("./checkout.route.js");
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware.js");
 const cartMiddleware = require("../../middlewares/client/cart.middleware.js");
@@ -13,4 +14,5 @@ module.exports = (app) => {
     app.use("/products", productRouters);
     app.use("/search", searchRouters);
     app.use("/cart", cartRouters);
+    app.use("/checkout", checkoutRouters);
 }
