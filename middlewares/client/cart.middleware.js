99859f08e22f274fedf6b8a768eadf5dff9e9d77
user020603 <ptit.tienthanh@gmail.com>
2024-02-25 15:26:26 +0700

    Lam nut mini cart tren header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -11,7 +11,13 @@ module.exports.cartId = async (req, res, next) => {
     });
   }
   else {
-    
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    })
+
+    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+
+    res.locals.cart = cart;
   }
   next();
 };
