788319503d012326b2a61378c5fb433483f1359e
user020603 <ptit.tienthanh@gmail.com>
2024-02-27 07:29:31 +0700

    404

--- index.js
+++ index.js
@@ -48,6 +48,14 @@ app.use(express.static(`${__dirname}/public`));
 route(app);
 routeAdmin(app);
 
+// 404 Not Found
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+  // res.redirect("/");
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
