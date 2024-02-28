b78dd2662aabc40f5abe7971ca0f2919513fadbb
user020603 <ptit.tienthanh@gmail.com>
2024-02-28 09:58:22 +0700

    Lam giao dien chat cho project

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,8 @@ const searchRouters = require("./search.route.js");
 const cartRouters = require("./cart.route.js");
 const checkoutRouters = require("./checkout.route.js");
 const userRouters = require("./user.route.js");
+const chatRouters = require("./chat.route.js");
+
 const settingMiddleware = require("../../middlewares/admin/setting.middleware.js");
 const categoryMiddleware = require("../../middlewares/client/category.middleware.js");
 const cartMiddleware = require("../../middlewares/client/cart.middleware.js");
@@ -20,4 +22,5 @@ module.exports = (app) => {
     app.use("/cart", cartRouters);
     app.use("/checkout", checkoutRouters);
     app.use("/user", userRouters);
+    app.use("/chat", chatRouters);
 }
