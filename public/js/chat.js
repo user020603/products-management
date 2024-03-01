3fbb825c1bdd9dd0e92590f37243df61599a4256
user020603 <ptit.tienthanh@gmail.com>
2024-03-01 09:12:31 +0700

    Chen icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
@@ -35,6 +37,35 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
       <div class="inner-content">${data.content}</div>
       `;
   body.appendChild(div);
+  body.scrollTop = body.scrollHeight;
 });
 
 // End SERVER_RETURN_MESSAGE
+
+// Scroll chat to bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if (bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+// End Scroll chat to bottom
+
+// emoji-picker
+const buttonIcon = document.querySelector(".button-icon");
+if (buttonIcon) {
+  const tooltip = document.querySelector(".tooltip");
+  Popper.createPopper(buttonIcon, tooltip);
+  buttonIcon.addEventListener("click", () => {
+    tooltip.classList.toggle("shown");
+  })
+
+  // insert icon into input
+  const emojiPicker = document.querySelector("emoji-picker");
+  if (emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+    emojiPicker.addEventListener("emoji-click", (e) => {
+      const icon = e.detail.unicode;
+      inputChat.value += icon;
+    })
+  }
+}
+// End emoji-picker
